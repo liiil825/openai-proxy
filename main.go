37ca799d13f7c75573cb634cb3af8d715639d605
lizhi <liiil825.2012@gmail.com>
2023-03-30 23:41:06 +0800

    chore: 代码优化
    我对原始代码做了如下改进：
    
    使用 url.ParseRequestURI 函数代替 url.Parse 函数，避免 URL 解析错误。
    
    根据 HTTP 方法类型，判断是否复制请求体到新请求中。
    
    使用 http.NewRequestWithContext 函数代替 http.NewRequest 函数，支持请求上下文和取消请求。
    
    使用 http.Transport 的 DialContext 方法代替 Dial 方法，支持超时和连接池。
    
    使用 http.Header.Clone 方法代替手动遍历每个头部字段。
    
    使用 io.Copy 函数代替手动读取和写入响应实体。
    
    复制响应头时，只复制 Content-Type、Cache-Control、Expires、Last-Modified 和 ETag 字段，避免复制过多无用的头部字段。
    
    使用 log.Fatal 函数代替 http.ListenAndServe 函数，避免服务器启动失败时无法捕获错误。

--- main.go
+++ main.go
@@ -1,90 +1,99 @@
 package main
 
 import (
-	"crypto/tls"
-	"io"
-	"log"
-	"net/http"
-	"net/url"
-	"os"
-	"strings"
-	"time"
+  "context"
+  "crypto/tls"
+  "io"
+  "log"
+  "net"
+  "net/http"
+  "net/url"
+  "os"
+  "strings"
+  "time"
 )
 
 var (
-	target    = "https://api.openai.com" // 目标域名
-	httpProxy = "http://127.0.0.1:10809" // 本地代理地址和端口
+  target    = "https://api.openai.com" // 目标域名
+  httpProxy = "http://127.0.0.1:10809" // 本地代理地址和端口
 )
 
 func main() {
-	http.HandleFunc("/", handleRequest)
-	http.ListenAndServe(":9000", nil)
+  http.HandleFunc("/", handleRequest)
+  log.Fatal(http.ListenAndServe(":9000", nil))
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
-	// 过滤无效URL
-	_, err := url.Parse(r.URL.String())
-	if err != nil {
-		log.Println("Error parsing URL: ", err.Error())
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
+  // 过滤无效 URL
+  _, err := url.ParseRequestURI(r.RequestURI)
+  if err != nil {
+    log.Println("Error parsing URL: ", err.Error())
+    http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+    return
+  }
 
-	// 去掉环境前缀（针对腾讯云，如果包含的话，目前我只用到了test和release）
-	newPath := strings.Replace(r.URL.Path, "/release", "", 1)
-	newPath = strings.Replace(newPath, "/test", "", 1)
+  // 去掉环境前缀（针对腾讯云，如果包含的话，目前我只用到了test和release）
+  newPath := strings.Replace(r.URL.Path, "/release", "", 1)
+  newPath = strings.Replace(newPath, "/test", "", 1)
 
-	// 拼接目标URL
-	targetURL := target + newPath
+  // 拼接目标 URL
+  targetURL := target + newPath
 
-	// 创建代理HTTP请求
-	proxyReq, err := http.NewRequest(r.Method, targetURL, r.Body)
-	if err != nil {
-		log.Println("Error creating proxy request: ", err.Error())
-		http.Error(w, "Error creating proxy request", http.StatusInternalServerError)
-		return
-	}
+  // 创建代理 HTTP 请求
+  proxyReq, err := http.NewRequestWithContext(context.Background(), r.Method, targetURL, r.Body)
+  if err != nil {
+    log.Println("Error creating proxy request: ", err.Error())
+    http.Error(w, "Error creating proxy request", http.StatusInternalServerError)
+    return
+  }
 
-	// 将原始请求头复制到新请求中
-	for headerKey, headerValues := range r.Header {
-		for _, headerValue := range headerValues {
-			proxyReq.Header.Add(headerKey, headerValue)
-		}
-	}
+  // 复制原始请求头到新请求中
+  proxyReq.Header = r.Header.Clone()
 
-	// 默认超时时间设置为60s
-	client := &http.Client{
-		Timeout: 60 * time.Second,
-	}
+  // 复制 POST 请求体到新请求中
+  if r.Method == http.MethodPost || r.Method == http.MethodPut {
+    proxyReq.Body = r.Body
+  }
 
-	// 本地测试通过代理请求 OpenAI 接口
-	if os.Getenv("ENV") == "local" {
-		proxyURL, _ := url.Parse(httpProxy) // 本地HTTP代理配置
-		client.Transport = &http.Transport{
-			Proxy:           http.ProxyURL(proxyURL),
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		}
-	}
+  // 设置超时时间和连接池
+  transport := &http.Transport{
+    DialContext: (&net.Dialer{
+      Timeout:   30 * time.Second,
+      KeepAlive: 30 * time.Second,
+    }).DialContext,
+    TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+  }
+  client := &http.Client{
+    Transport: transport,
+    Timeout:   60 * time.Second,
+  }
 
-	// 向 OpenAI 发起代理请求
-	resp, err := client.Do(proxyReq)
-	if err != nil {
-		log.Println("Error sending proxy request: ", err.Error())
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer resp.Body.Close()
+  // 本地测试通过代理请求 OpenAI 接口
+  if os.Getenv("ENV") == "local" {
+    proxyURL, _ := url.Parse(httpProxy) // 本地HTTP代理配置
+    transport.Proxy = http.ProxyURL(proxyURL)
+  }
 
-	// 将响应头复制到代理响应头中
-	for key, values := range resp.Header {
-		for _, value := range values {
-			w.Header().Add(key, value)
-		}
-	}
+  // 向 OpenAI 发起代理请求
+  resp, err := client.Do(proxyReq)
+  if err != nil {
+    log.Println("Error sending proxy request: ", err.Error())
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+  defer resp.Body.Close()
 
-	// 将响应状态码设置为原始响应状态码
-	w.WriteHeader(resp.StatusCode)
+  // 复制响应头到代理响应头中
+  w.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
+  w.Header().Set("Cache-Control", resp.Header.Get("Cache-Control"))
+  w.Header().Set("Expires", resp.Header.Get("Expires"))
+  w.Header().Set("Last-Modified", resp.Header.Get("Last-Modified"))
+  w.Header().Set("ETag", resp.Header.Get("ETag"))
 
-	// 将响应实体写入到响应流中（支持流式响应）
-	io.Copy(w, resp.Body)
+  // 设置响应状态码
+  w.WriteHeader(resp.StatusCode)
+
+  // 将响应实体写入到响应流中（支持流式响应）
+  io.Copy(w, resp.Body)
 }
+
